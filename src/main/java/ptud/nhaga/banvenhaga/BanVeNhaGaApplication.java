c92fadef387a5c96a87b9bf09e6fd1876e9aa7d6
anhduc0912 <phamquocanhduc2003hn@gmail.com>
2024-02-19 15:58:03 +0700

    first commit

---
+++ src/main/java/ptud/nhaga/banvenhaga/BanVeNhaGaApplication.java
@@ -0,0 +1,13 @@
+package ptud.nhaga.banvenhaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanVeNhaGaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanVeNhaGaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ptud/nhaga/banvenhaga/BanVeNhaGaApplicationTests.java
@@ -0,0 +1,13 @@
+package ptud.nhaga.banvenhaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanVeNhaGaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
